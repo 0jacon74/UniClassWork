6b0dd9ddbd1e4cce07c36b8ebbde597797db2b1e
0jacon74 <0jacon74@solent.ac.uk>
2023-10-03 12:40:17 +0100

    Demonstration of a more complex if statement

--- Week2/if_statements.py
+++ Week2/if_statements.py
@@ -6,15 +6,27 @@
 #    print("Performing activity...")
 #print("Activity Completed!")
 
-print("What direction should I move in (up, down, left, right)?")
-direction = input()
-if direction == "up":
-    print ("I am moving in an upward direction!")
-elif direction == "down":
-    print ("I am moving in an downward direction!")
-elif direction == "left":
-    print ("I am moving left!")
-elif direction == "right":
-    print ("I am moving right!")
-else:
-    print ("That is not a direction!")
+#print("What direction should I move in (up, down, left, right)?")
+#direction = input()
+#if direction == "up":
+#    print ("I am moving in an upward direction!")
+#elif direction == "down":
+#    print ("I am moving in an downward direction!")
+#elif direction == "left":
+#    print ("I am moving left!")
+#elif direction == "right":
+#    print ("I am moving right!")
+#else:
+#    print ("That is not a direction!")
+
+print("please enter a first number")
+firstnumber = input()
+print("please enter a second number")
+secondnumber = input()
+
+if firstnumber > secondnumber:
+    print ("first number is bigger")
+elif secondnumber > firstnumber:
+    print ("second number is bigger")
+elif secondnumber == firstnumber:
+    print ("Both numbers are equal")
