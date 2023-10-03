c59f3e2e8d64e6883087b7530a81c6377f241e8e
0jacon74 <0jacon74@solent.ac.uk>
2023-10-03 12:10:08 +0100

    Code to demonstrate an IF statement

---
+++ Week2/if_statements.py
@@ -0,0 +1,7 @@
+print("Please enter an activity")
+answer = input()
+if answer == "Calculate":
+    print("Performing calculations...")
+else:
+    print("Performing activity...")
+print("Activity Completed!")
