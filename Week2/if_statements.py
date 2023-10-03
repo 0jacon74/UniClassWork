a721942fa6525d632c46813ceab18909362a1f99
0jacon74 <0jacon74@solent.ac.uk>
2023-10-03 12:22:49 +0100

    Demonstration of a more complex if statement

--- Week2/if_statements.py
+++ Week2/if_statements.py
@@ -1,7 +1,20 @@
-print("Please enter an activity")
-answer = input()
-if answer == "Calculate":
-    print("Performing calculations...")
+#print("Please enter an activity")
+#answer = input()
+#if answer == "Calculate":
+#    print("Performing calculations...")
+#else:
+#    print("Performing activity...")
+#print("Activity Completed!")
+
+print("What direction should I move in (up, down, left, right)?")
+direction = input()
+if direction == "up":
+    print ("I am moving in an upward direction!")
+elif direction == "down":
+    print ("I am moving in an downward direction!")
+elif direction == "left":
+    print ("I am moving left!")
+elif direction == "right":
+    print ("I am moving right!")
 else:
-    print("Performing activity...")
-print("Activity Completed!")
+    print ("That is not a direction!")
