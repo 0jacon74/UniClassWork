4d798f18e640d0c253427d1181b1cead52cfad93
0jacon74 <0jacon74@solent.ac.uk>
2023-10-10 12:34:43 +0100

    While loop example

--- Week 3/while_loop.py
+++ Week 3/while_loop.py
@@ -1,9 +1,20 @@
 
-avoid = input("How many obstacles must I avoid?")
-obstacles = 0
+#avoid = input("How many obstacles must I avoid?")
+#obstacles = 0
 
-while obstacles < int(avoid):
-    obstacles = obstacles + 1
-    print ("Avoiding... Done! "+ str(obstacles) + " obstacles avoided.")
+#while obstacles < int(avoid):
+#    obstacles = obstacles + 1
+#    print ("Avoiding... Done! "+ str(obstacles) + " obstacles avoided.")
+#
+#print("All obstacles have been avoided.")
 
-print("All obstacles have been avoided.")
+
+answer = input("Please enter a phrase: ")
+
+repeat = 0
+
+while repeat < len(answer):
+    repeat = repeat + 1
+    print ("Hi ", end = "")
+
+
