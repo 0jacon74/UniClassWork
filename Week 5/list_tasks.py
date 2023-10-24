6652aab3c755ea2d2c9d4316f6f32fa1894af47f
0jacon74 <0jacon74@solent.ac.uk>
2023-10-24 11:28:43 +0100

    Basic List in a function

---
+++ Week 5/list_tasks.py
@@ -0,0 +1,8 @@
+def directions(d1, d2, d3, d4):
+    steps = [d1, d2, d3, d4]
+    print(steps)
+
+def run_task_1():
+    directions("Move Forward", "Move Backwards", "Turn Left", "Turn Right")
+
+run_task_1()
